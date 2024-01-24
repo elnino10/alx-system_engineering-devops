805f57cdfc37dbcc05c4cc8af3f2a775c21c882c
Joe Egboka <joeegboka@gmail.com>
2024-01-25 00:52:54 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,15 @@
 # Install `flask` from `pip3`
 # flask version must be `2.1.0`
- 
-$flask_package = 'flask'
-$flask_version = '2.1.0'
-$werkzeug_package = 'werkzeug'
-$werkzeug_version = '2.1.1'
 
 # install flask with version 2.1.0
-package { $flask_package:
-  ensure   => $flask_version,
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
 
 # install werkzeug
-package { $werkzeug_package:
-  ensure   => $werkzeug_version,
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
 
