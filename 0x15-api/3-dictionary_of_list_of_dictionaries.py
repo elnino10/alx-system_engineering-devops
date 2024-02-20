ce1f49505e4501e2d60af212e5a7fc0c814fdf25
Joe Egboka <joeegboka@gmail.com>
2024-02-20 13:22:42 +0100

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,34 +5,6 @@ import json
 
 import requests
 
-# if __name__ == '__main__':
-#     url = "https://jsonplaceholder.typicode.com/users"
-
-#     resp = requests.get(url)
-#     Users = resp.json()
-
-#     users_dict = {}
-#     for user in Users:
-#         USER_ID = user.get('id')
-#         USERNAME = user.get('username')
-#         url = 'https://jsonplaceholder.typicode.com/users/{}'.format(USER_ID)
-#         url = url + '/todos/'
-#         resp = requests.get(url)
-
-#         tasks = resp.json()
-#         users_dict[USER_ID] = []
-#         for task in tasks:
-#             TASK_COMPLETED_STATUS = task.get('completed')
-#             TASK_TITLE = task.get('title')
-#             users_dict[USER_ID].append({
-#                 "task": TASK_TITLE,
-#                 "completed": TASK_COMPLETED_STATUS,
-#                 "username": USERNAME
-#             })
-#     with open('todo_all_employees.json', 'w') as f:
-#         json.dump(users_dict, f)
-
-
 if __name__ == "__main__":
     REST_API = "https://jsonplaceholder.typicode.com/"
     users = requests.get(f"{REST_API}users/", timeout=10).json()
@@ -41,7 +13,8 @@ if __name__ == "__main__":
     for user in users:
         user_id = user.get("id")
         username = user.get("username")
-        user_task_req = requests.get(f"{REST_API}/users/{user_id}/todos").json()
+        user_task_req = requests.get(
+            f"{REST_API}/users/{user_id}/todos", timeout=10).json()
         todo_dict[user_id] = []
         for task in user_task_req:
             todo_dict[user_id].append({
