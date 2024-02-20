4664deeb7968c42ec4ad8d5b80d1f79e0a030d6c
Joe Egboka <joeegboka@gmail.com>
2024-02-20 06:48:57 +0100

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,45 +4,42 @@ script that uses the https://jsonplaceholder.typicode.com/ REST API,
 for a given employee ID, returns information about his/her TODO list
 progress and extend script to export data in the CSV format
 """
-import requests
 import sys
 
-if __name__ == '__main__':
-    user = sys.argv[1]
-    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
-    res = requests.get(url_user)
-    """ANYTHING"""
-    user_name = res.json().get('username')
-    task = url_user + '/todos'
-    res = requests.get(task)
-    tasks = res.json()
+import requests
 
-    with open('{}.csv'.format(user), 'w') as csvfile:
-        for task in tasks:
-            completed = task.get('completed')
-            """Complete"""
-            title_task = task.get('title')
-            """Done"""
-            csvfile.write('"{}","{}","{}","{}"\n'.format(
-                user, user_name, completed, title_task))
+# if __name__ == '__main__':
+#     user = sys.argv[1]
+#     url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+#     res = requests.get(url_user)
+#     """ANYTHING"""
+#     user_name = res.json().get('username')
+#     task = url_user + '/todos'
+#     res = requests.get(task)
+#     tasks = res.json()
 
-# REST_API = "https://jsonplaceholder.typicode.com/"
-# if __name__ == "__main__":
-#     if len(sys.argv) > 1:
-#         if isinstance(sys.argv[1].isnumeric(), int):
-#             user_id = int(sys.argv[1])
-#             user = requests.get(
-            # f"{REST_API}users/{user_id}", timeout=5).json()
-#             user_tasks = requests.get(
-#                 f"{REST_API}users/{user_id}/todos", timeout=5).json()
-#             user_name = user["name"]
+#     with open('{}.csv'.format(user), 'w') as csvfile:
+#         for task in tasks:
+#             completed = task.get('completed')
+#             """Complete"""
+#             title_task = task.get('title')
+#             """Done"""
+#             csvfile.write('"{}","{}","{}","{}"\n'.format(
+#                 user, user_name, completed, title_task))
 
-#             csv_file = f"{user_id}.csv"
-#             # "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-#             task_list = []
-#             with open(csv_file, 'w', encoding="utf-8") as c_file:
-#                 for task in user_tasks:
-#                     list_format = f"\"{user_id}\",\"{user['name']}\",\
-            # "{user_tasks['completed']}\",\"{user_tasks['title']}\""
-#                     task_list.append(list_format)
-#                     c_file.write(task_list)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        REST_API = "https://jsonplaceholder.typicode.com/"
+        if isinstance(sys.argv[1].isnumeric(), int):
+            user_id = int(sys.argv[1])
+            user = requests.get(f"{REST_API}users/{user_id}", timeout=5).json()
+            user_tasks = requests.get(
+                f"{REST_API}users/{user_id}/todos", timeout=5).json()
+
+            csv_file = f"{user_id}.csv"
+            with open(csv_file, 'w', encoding="utf-8") as c_file:
+                for task in user_tasks:
+                    list_format = f"\"{user_id}\",\"{user['username']}\",\""
+                    list_format += f"{task['completed']}\",\"{task['title']}"
+                    list_format += "\"\n"
+                    c_file.write(list_format)
