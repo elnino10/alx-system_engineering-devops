3a7d47817700d2a47faf9816fb0b2a45ffa4f225
Joe Egboka <joeegboka@gmail.com>
2024-03-16 22:32:38 +0100

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# configure nginx web server to serve and accept requests
+
+exec {'modify limit':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
+
